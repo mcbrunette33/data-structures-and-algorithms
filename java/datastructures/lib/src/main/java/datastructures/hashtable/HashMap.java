c91f55f5b28f33639a5762ccb275f3a237d1e5bf
mcbrunette33 <mcbrunette33@gmail.com>
2022-06-21 16:18:51 -0700

    added files for hash

---
+++ java/datastructures/lib/src/main/java/datastructures/hashtable/HashMap.java
@@ -0,0 +1,11 @@
+package datastructures.hashtable;
+
+import java.util.ArrayList;
+
+
+public class HashMap <K, V> {
+
+  public void set (K key, V value){
+
+  }
+}
---
+++ java/datastructures/lib/src/test/java/datastructures/hashtabletest/HashMapTest.java
@@ -0,0 +1,4 @@
+package datastructures.hashtabletest;
+
+public class HashMapTest {
+}
