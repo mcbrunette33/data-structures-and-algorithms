6a7ae808ba967de00a16a2b2eca5a339504e5fec
mcbrunette33 <mcbrunette33@gmail.com>
2022-06-22 19:13:14 -0700

    added repeatword

--- java/datastructures/lib/src/main/java/datastructures/hashtable/HashMap.java
+++ java/datastructures/lib/src/main/java/datastructures/hashtable/HashMap.java
@@ -1,11 +1,41 @@
 package datastructures.hashtable;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class HashMap <K, V> {
+  ArrayList<LinkedList<HashMapPair<K, V>>> buckets;
+  int size;
+  public HashMap(int size){
+    if(size < 1){
+      throw new IllegalArgumentException("Hashmap size must be greater than 1.");
+    }
 
-  public void set (K key, V value){
+    this.size = size;
+    this.buckets = new ArrayList<>(size);
+    for(int i = 0; i < this.size; i++){
+      buckets.add(i, new LinkedList<>());
+    }
+  }
+  public void set (){
 
+  }
+  public void add (){
+
+  }
+  public boolean contains (){
+  }
+  public String repeatedWord(String text, HashMap<String, Integer> hashmap){
+    String[] newArray = text.split("\\W+");
+    for(int i = 0; i < newArray.length; i++){
+      if(hashmap.contains(newArray[i])){
+        return newArray[i];
+      }
+      else{
+        hashmap.add(newArray[i].toLowerCase(),i);
+      }
+    }
+    return "No Repeated Strings";
   }
 }
---
+++ java/datastructures/lib/src/main/java/datastructures/hashtable/HashMapPair.java
@@ -0,0 +1,15 @@
+package datastructures.hashtable;
+
+import java.util.AbstractMap;
+
+public class HashMapPair<K, V> extends AbstractMap.SimpleEntry<K, V> implements Comparable<HashMapPair<K, V>> {
+
+  public HashMapPair(K key, V value){
+    super(key, value);
+  }
+
+  @Override
+  public int compareTo(HashMapPair<K, V> o) {
+    throw new UnsupportedOperationException("doesnt work");
+  }
+}
